1b2039a1696dd9d620a851d7206429c4707543f6
salmanfairoze <salman.fairoze28@gmail.com>
2019-11-21 00:19:08 +0530

    running code to load database and schema to hdfs

--- hdfs-load.py
+++ hdfs-load.py
@@ -4,6 +4,7 @@
 
 # import the python subprocess module
 import subprocess
+import time
 from pydoop import hdfs
 
 
@@ -19,12 +20,24 @@ def run_cmd(args_list):
 	s_return =  proc.returncode
 	return s_return, s_output, s_err
 
-db,data_file,schema = input()
+db = input()
+data_file = input()
+file_name = data_file.split("/")[-1]
+schema = input()+"\n"
 (ret, out, err) = run_cmd(['hdfs','dfs','-ls','/sql/'])
+out = out.decode()
 if not(ret) and out.find(db) != -1:
 	print("database already exists")
 elif not(ret) and out.find(db) == -1:
 	run_cmd(['hdfs','dfs','-mkdir','/sql/'+db])
-	run_cmd(['hdfs','dfs','-put','/sql/'+db+db])
-	with hdfs.open('/sql/'+db+'/'+db, 'a') as f:
-        f.write(data.encode())
+	run_cmd(['hdfs','dfs','-put',data_file,'/sql/'+db+'/'])
+	#time.sleep(2)
+	run_cmd(['hdfs','dfs','-mv',"/sql/"+db+"/"+file_name,'/sql/'+db+'/'+db+".csv"])
+	run_cmd(['hdfs','dfs','-touch','/sql/'+db+'/'+db+"_scheme.csv"])
+	try:
+		with hdfs.open('/sql/'+db+'/'+db+"_scheme.csv", 'a') as f:
+			data = schema
+			f.write(data.encode())
+	except:
+		pass
+
