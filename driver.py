d1a1fe255d0e776d09924d17ae3eb91c48fe25ab
salmanfairoze <salman.fairoze28@gmail.com>
2019-11-30 00:41:44 +0530

    Minor bug fixes

--- driver.py
+++ driver.py
@@ -1,19 +1,37 @@
+import load
+import parser
+import delete
+import read
+import os
+
 while 1:
-	print("crab>>",end=" ")
+	print("-->",end=" ")
 	query  = input();
 	if(query=="exit"):
 		break;
-	queries = parser(query)
-	
-	list_queries = queries.keys()
+	queries = parser.parser(query)
+	if(queries==-1):
+		continue
+	# print(queries)
+	# print(queries)
+	db_name = queries['database']
+	table_name = queries['table'].strip(".csv")
+	del queries['database']
+	del queries['table']
+	list_queries = list(queries.keys())
+	# print(db_name,table_name,list_queries)
 
+	# print(list_queries)
 	if(len(list_queries)==1):
 		 if('load' in list_queries):
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system("python3 hdfs-load-Webhdfs.py " + queries['load'])
+		 			# print(queries['load'])
+		 			db_name,table_name,schema=queries['load'].split()
+		 			load.load_data(db_name,table_name,schema)
 		 		else:
 		 			os.wait()
+
 
 		 elif('delete' in list_queries):
 		 		new_proc = os.fork()
@@ -25,68 +43,73 @@ while 1:
 		 elif('project' in list_queries):
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar\
-		 						-D mapred.reduce.tasks=0\
-		 						-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt"' + \
-		 						'-file select_mapper.py \
-		 						-mapper "python3 select_mapper.py ' + queries['select'] + \
-		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv' + \
-		 						'-output /sql/final_out')
+		 			cmd = 'hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 						'-D mapred.reduce.tasks=0 ' + \
+		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
+		 						'-file project_mapper.py '+ \
+		 						'-mapper "python3 project_mapper.py ' + queries['project'] + '" '+ \
+		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
+		 						'-output /sql/final_out'
+
+		 			# print(cmd)
+		 			os.system(cmd)
 		 		else:
 		 			os.wait()
 	else:
 		if(('select' in list_queries) and ('aggregate' in list_queries)):   #SELECT COL1 FROM DB/T.CSV WHERE COL1=VAL1 AGGREGATE BY COUNT;"
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 			# print(db_name,table_name)
+		 			cmd = 'hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
 		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
 		 						'-file select_mapper.py ' + \
-		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '" ' + \
+		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '\" ' + \
 		 						'-file agg_reducer.py ' + \
-		 						'-reducer "python3 agg_reducer.py'   + queries['aggregate'] + '" ' + \
-		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
-		 						'-output /sql/final_out')
+		 						'-reducer "python3 agg_reducer.py'   + " " + queries['aggregate'][0] + "," +queries['aggregate'][1] + '\" ' + \
+		 						'-input /sql/' + str(db_name) + '/' + str(table_name) + '/' + str(table_name) + '.csv ' + \
+		 						'-output /sql/final_out'
+		 			# print(cmd)
+		 			os.system(cmd)
 		 		else:
 		 			os.wait()
 		elif(('select' in list_queries) and ('project' in list_queries)):  #SELECT COL1 FROM DB/T.CSV WHERE COL1=VAL1;
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' \
+		 			cmd = 'hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' \
 		 						'-D mapred.reduce.tasks=0 ' + \
 		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
-		 						'-file select_mapper.py' + \
-		 						'-mapper "python3 select_mapper.py ' + queries['select'] '" ' + \
+		 						'-file select_mapper.py ' + \
+		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '\" ' + \
 		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
-		 						'-output /sql/temp/select_out')
-
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 						'-output /sql/temp/select_out'
+		 			# print(cmd)
+		 			os.system(cmd)
+		 			cmd = 'hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
 		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
 		 						'-file project_mapper.py ' + \
-		 						'-mapper "python3 project_mapper.py ' + queries['project'] '" '+ \
+		 						'-mapper "python3 project_mapper.py ' + queries['project'] + '\" '+ \
 		 						'-file remdup_reducer.py '+ \
 		 						'-reducer "python3 remdup_reducer.py" ' + \
 		 						'-input /sql/temp/select_out ' + \
-		 						'-output /sql/final_out')
+		 						'-output /sql/final_out'
+		 			os.system(cmd)
 		 		else:
 		 			os.wait()
 		elif(('project' in list_queries) and ('aggregate' in list_queries)):
-		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+				new_proc = os.fork()
+				if(new_proc == 0):
+		 			cmd = 'hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
 		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
 		 						'-file identity_mapper.py ' + \
-		 						'-mapper "python3 identity_mapper.py ' + queries['select'] + '" ' + \
+		 						'-mapper "python3 identity_mapper.py ' + queries['project'] + '\" ' + \
 		 						'-file agg_reducer.py ' + \
-		 						'-reducer "python3 agg_reducer.py'   + queries['aggregate'] + '" ' + \
+		 						'-reducer "python3 agg_reducer.py'   + " " + queries['aggregate'][0] + "," +queries['aggregate'][1] + '\" ' + \
 		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
-		 						'-output /sql/final_out')
-		 		else:
+		 						'-output /sql/final_out'
+		 			print(cmd)
+		 			os.system(cmd)
+				else:
 		 			os.wait()
-
-<<<<<<< HEAD
-		exit();
-	l=parser(query)
-=======
-
-	
-
->>>>>>> 1013ba79731626cf1b61c77c4da9745baf7ef4da
+		delete.delete_folder("/sql/temp/select_out/ ")
+		delete.delete_folder("/sql/final_out/ ")
+		 		
