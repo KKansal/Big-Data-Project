ce31ca1e4852f1b0db8662681e3ec05ce241c43e
salmanfairoze <salman.fairoze28@gmail.com>
2019-11-29 19:09:56 +0530

    Added delete REST api

--- delete.py
+++ delete.py
@@ -1,15 +1,10 @@
-#a=delete("http://localhost:9870/webhdfs/v1/sql/t1/?user.name=hadoop&op=DELETE&recursive=true")
-
 from requests import delete
 from getpass import getuser
-username = getuser()
 
 file_path = input()
 file_path=file_path.strip()
 
 respons = delete("http://localhost:9870/webhdfs/v1/"+file_path+"?user.name="+getuser()+"&op=DELETE&recursive=true")
-
-#print(respons.json())
 
 if respons.json()['boolean'] == True:
 	print("File/folder deleted successfully")
