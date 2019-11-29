68a4685d6ae9acf985e40bc50f328bd658076956
Keshav Kansal <kansalkeshav@gmail.com>
2019-11-29 21:36:21 +0530

    Modification

--- testing/driver.py
+++ testing/driver.py
@@ -1,21 +1,30 @@
-
+import load
+import parser
+import os
 
 while 1:
 	print("-->",end=" ")
 	query  = input();
 	if(query=="exit"):
 		break;
-	queries = parser(query)
-	
-	list_queries = queries.keys()
+	queries = parser.parser(query)
+	# print(queries)
+	db_name = queries['data']
+	table_name = queries['table']
+	list_queries = list(queries.keys())
 
+
+	print(list_queries)
 	if(len(list_queries)==1):
 		 if('load' in list_queries):
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system("python3 hdfs-load-Webhdfs.py " + queries['load'])
+		 			# print(queries['load'])
+		 			db_name,table_name,schema=queries['load'].split()
+		 			load.load_data(db_name,table_name,schema)
 		 		else:
 		 			os.wait()
+
 
 		 elif('delete' in list_queries):
 		 		new_proc = os.fork()
@@ -24,7 +33,7 @@ while 1:
 		 		else:
 		 			os.wait()
 
-		 elif('select' in list_queries):
+		 elif('project' in list_queries):
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
 		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar\
@@ -33,36 +42,54 @@ while 1:
 		 						'-file select_mapper.py \
 		 						-mapper "python3 select_mapper.py ' + queries['select'] + \
 		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv' + \
-		 						'-output /sql/finalout')
+		 						'-output /sql/final_out')
 		 		else:
 		 			os.wait()
 	else:
-		if('select' in list_queries) and ('aggregate' in list_queries):
+		if(('select' in list_queries) and ('aggregate' in list_queries)):   #SELECT COL1 FROM DB/T.CSV WHERE COL1=VAL1 AGGREGATE BY COUNT;"
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar' \
-		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt"' + \
-		 						'-file select_mapper.py' \
-		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '"' + \
-		 						'-file agg_reducer.py'
-		 						'-reducer "python3 agg_reducer.py'   + queries['aggregate']
-		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv' + \
-		 						'-output /sql/temp/select_out')
+		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
+		 						'-file select_mapper.py ' + \
+		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '\" ' + \
+		 						'-file agg_reducer.py ' + \
+		 						'-reducer "python3 agg_reducer.py'   + queries['aggregate'] + '\" ' + \
+		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
+		 						'-output /sql/final_out')
 		 		else:
 		 			os.wait()
-		if('aggregate' in list_queries):
+		elif(('select' in list_queries) and ('project' in list_queries)):  #SELECT COL1 FROM DB/T.CSV WHERE COL1=VAL1;
 		 		new_proc = os.fork()
 		 		if(new_proc == 0):
-		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar\
-		 						-D mapred.reduce.tasks=0\
-		 						-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt"' + \
-		 						'-file select_mapper.py \
-		 						-mapper "python3 select_mapper.py ' + queries['select'] + \
-		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv' + \
+		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' \
+		 						'-D mapred.reduce.tasks=0 ' + \
+		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
+		 						'-file select_mapper.py' + \
+		 						'-mapper "python3 select_mapper.py ' + queries['select'] + '\" ' + \
+		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
 		 						'-output /sql/temp/select_out')
+
+		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
+		 						'-file project_mapper.py ' + \
+		 						'-mapper "python3 project_mapper.py ' + queries['project'] + '\" '+ \
+		 						'-file remdup_reducer.py '+ \
+		 						'-reducer "python3 remdup_reducer.py" ' + \
+		 						'-input /sql/temp/select_out ' + \
+		 						'-output /sql/final_out')
 		 		else:
 		 			os.wait()
-	
-
-
-
+		elif(('project' in list_queries) and ('aggregate' in list_queries)):
+		 		if(new_proc == 0):
+		 			os.system('hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar ' + \
+		 						'-files "hdfs://localhost:9000/sql/' + db_name + '/' + table_name + '/schema.txt" ' + \
+		 						'-file identity_mapper.py ' + \
+		 						'-mapper "python3 identity_mapper.py ' + queries['select'] + '\" ' + \
+		 						'-file agg_reducer.py ' + \
+		 						'-reducer "python3 agg_reducer.py'   + queries['aggregate'] + '\" ' + \
+		 						'-input /sql/' + db_name + '/' + table_name + '/' + table_name + '.csv ' + \
+		 						'-output /sql/final_out')
+		 		else:
+		 			os.wait()
+		 		
---
+++ testing/parser.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 14:08:15 2019
+
+@author: Hp
+"""
+
+import re
+def parser(query):
+    # query=input("Enter your query:")
+    regex_load=r'(load|LOAD) \w+/\w+\.csv (AS|as)\s\(([a-z A-z]+:(string|int|integer|char|varchar|float|double|time|year)(,|))+\);'
+    regex_select=r'(select|SELECT)\s(\w+|\w+,)\s(from|FROM)\s\w+/\w+\.csv(\s(where|WHERE)\s\w+(\s|)=(\s|)([0-9]+|\w+|)|);'
+    #regex_delete=r'(delete|DELETE)\s(\*|[a-z A-Z 0-9]+)\s(from|FROM)\s[a-z A-Z 0-9]+/[a-z A-Z 0-9]+\.csv(\s(where|WHERE)\s[a-z A-Z 0-9]+(\s|)=(\s|)([0-9]+|\'[a-z A-Z]+\')|);'
+    regex_delete=r'(DELETE|delete)\s\w+\/\w+\.csv;'
+    regex_aggr=r'(SELECT|select)\s\w+\s(from|FROM)\s\w+/\w+\.csv((\s(where|WHERE) \w+=([0-9]+|\w+|))|) (AGGREGATE|aggregate) by (COUNT|SUM|MAX|MIN|count|sum|max|min);'
+    if(re.search(regex_load,query)!=None):
+        query=query.split()
+        d={}
+        database=query[1].split("/")[0]
+        table=query[1].split("/")[1]
+        if("AS" in query):
+          index_as=query.index("AS")
+        else:
+             index_as=query.index("as")
+        schema=query[index_as+1].split(";")[0]
+        d["load"]=database+" "+table+" "+schema
+        return(d)
+    elif(re.search(regex_select,query)!=None):
+        d={}
+        query=query.split()
+        
+        if('where' in query):
+            index=query.index('where')
+            #l1.append(query[index+1].split(";")[0])
+            d['select']=query[index+1].split(";")[0]
+        elif('WHERE' in query):
+            index=query.index('WHERE')
+            d['select']=query[index+1].split(";")[0]
+        d['project']=query[1]
+        return(d)
+    elif(re.search(regex_delete,query)!=None):
+        d={}
+        query=query.split()
+        d['delete']=query[1].split("/")[0]+" "+query[1].split("/")[1].split(";")[0]
+        return(d)
+        
+    elif(re.search(regex_aggr,query)!=None):
+        d={}
+        query=query.split()
+        if('where' in query):
+            index=query.index('where')
+            d[query[0]]=query[index+1].split(";")[0]
+            
+        elif('WHERE' in query):
+            index=query.index('WHERE')
+            d[query[0]]=query[index+1].split(";")[0]
+        d['project']=query[1]
+        if("AGGREGATE" in query):
+            index_aggr=query.index("AGGREGATE")
+            
+        else:
+            
+            index_aggr=query.index("aggregate")
+        #l3.extend([string,query[1],query[index_aggr+2].split(";")[0]])
+        string="aggregate"
+        d[string]=[query[1],query[index_aggr+2].split(";")[0]]
+        return(d)
+    else:
+        print("error: Please check u might have missed table name or semicolon or check syntax")
+        exit()
